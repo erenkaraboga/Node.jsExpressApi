002370d6aae3a42d1db583b88397123902c2db0e
erenkaraboga <darkbull1073@gmail.com>
2022-09-02 11:20:27 +0300

    Product Route Added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const express = require('express');
+const productsRouter = require("./routes/products");
 const app = express();
+
+app.use("/products", productsRouter);
+
 app.listen(5000, () => console.log('Server started'));
