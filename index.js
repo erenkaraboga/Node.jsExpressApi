e2f92b06066a64784fb9e010d5f53ea846e06d39
erenkaraboga <darkbull1073@gmail.com>
2022-09-02 19:49:04 +0300

    Product Api Project Finished.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const express = require('express');
+const express = require("express");
+const authRouter = require("./routes/auth");
 const productsRouter = require("./routes/products");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const verifyToken = require("./middleware/verifyToken");
+
 const app = express();
 
-app.use("/products", productsRouter);
+app.use(bodyParser.json());
 
-app.listen(5000, () => console.log('Server started'));
+app.use(cors());
+
+mongoose.connect(
+  `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xnfsv.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+  (e) => {
+    if (e) {
+      console.log(e);
+    } else {
+      console.log("Connected to database");
+    }
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hi, welcome to Products RESTFUL API 😍");
+});
+
+app.use("/auth", authRouter);
+app.use("/products", verifyToken, productsRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,61 @@
 const express = require("express");
-
+const Product = require("../models/Product");
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send("products");
+
+router.get("/", (req, res) => {
+  Product.find()
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
+
+router.get("/:id", (req, res) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      res.json(product);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    user: req.userId,
+  });
+  product.save();
+  res.json(product);
+});
+
+router.put("/:id", (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  })
+    .then((product) => {
+      res.json(product);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then((product) => {
+      res.json(product);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
